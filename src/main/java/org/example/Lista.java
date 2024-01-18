236c03dfee64ca1e313b0860621f5fe3890b6966
alorenzot <alejandrolorenzotoledo@gmail.com>
2024-01-18 21:15:59 +0100

    ejercicios

--- src/main/java/org/example/Lista.java
+++ src/main/java/org/example/Lista.java
@@ -5,7 +5,36 @@ public class Lista {
     private Nodo head;
     private Nodo tail;
 
+    public Lista(){
+        size=0;
+        head=null;
+        tail=null;
+    }
+    public void addHead(int num){
+        Nodo nodo = new Nodo(num);
+        if (size==0) {
+            head = nodo;
+            tail = nodo;
+            size++;
+        } else {
+            nodo.setNext(head);
+            head=nodo;
+            size++;
+        }
+    }
 
+    @Override
+    public String toString() {
+        Nodo n = head;
+        String aux="Size: " + size + "\n" +
+                "Values: ";
+        while (n!=null){
+            aux+=n.getInfo()+" ";
+            n=n.getNext();
+
+        }
+        return aux;
+    }
 
     private class Nodo{
         private int info;
@@ -13,6 +42,15 @@ public class Lista {
         public Nodo(int info){
             this.info=info;
             this.next=null;
+        }
+        public int getInfo() {
+            return info;
+        }
+        public Nodo getNext() {
+            return next;
+        }
+        public void setNext(Nodo next) {
+            this.next = next;
         }
     }
 }
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Lista lista = new Lista();
+        lista.addHead(3);
+        lista.addHead(6);
+        lista.addHead(5);
+        lista.addHead(1);
+
+        System.out.println(lista);
+    }
+}
